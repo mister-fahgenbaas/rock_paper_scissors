126d2a1b534c41dbde6405fbae24ddcaed9a8c7b
mister-fahgenbaas <87523979+mister-fahgenbaas@users.noreply.github.com>
2023-05-01 17:44:53 +0100

    Change play() to playRound(). Incorporate five rounds of playing.

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-function getComputerChoice() {
+function getComputerSelection() {
 	let randomIndex = Math.floor( Math.random() * 3 );  // 0 <= randomIndex < 3
-	let randomChoice = CHOICES[randomIndex];
-	return randomChoice;
+	let randomSelection = CHOICES[randomIndex];
+	return randomSelection;
 }
 
-function play(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
 	playerSelection = playerSelection.toUpperCase();
 	let resultMessage;
 	let resultMessages = {
-		"win": "You win!",
-		"lose": `You lose. ${computerSelection} beats ${playerSelection}`,
-		"draw": "It's a draw"
+		"win": `You win a point! ${playerSelection} beats ${computerSelection}`,
+		"lose": `You lose a point. ${computerSelection} beats ${playerSelection}`,
+		"draw": "It's a draw."
 	};
 
 	if (
@@ -27,11 +27,11 @@ function play(playerSelection, computerSelection) {
 			resultMessage = resultMessages["lose"];
 	else
 			resultMessage = resultMessages["draw"];
-	 
+	//console.table([playerSelection, computerSelection]);
 	return resultMessage;
 }
 
-function getPlayerChoice() {
+function getPlayerSelection() {
 	let playerSelection = "";
 	do  {
 		playerSelection = prompt("Enter a choice from rock, paper, and scissors : ").toString();
@@ -40,37 +40,35 @@ function getPlayerChoice() {
 	return playerSelection;	
 }
 
-function main() {
-
-
-	let playerSelection = ""; 
+function game() {
+	let computerScore = 0;
+	let playerScore = 0;
+	let roundsToPlay = 5;
+	let playerSelection = "";
 	let computerSelection = "";
-	let playing = true
-	let shallWePlayAgain = "false"
 
-	while (playing) {
-
-		playerSelection = getPlayerChoice();
-		computerSelection = getComputerChoice();
-
-		console.log( 
-`***********************************************\n \
-	You have chosen ${playerSelection.toUpperCase()}\n \
-	CPU has chosed ${computerSelection}\n \
-	${play(playerSelection, computerSelection)}\n \
-************************************************
-		` 
-		);
-		playing = prompt("PLAY AGAIN?\n(Y/N):") == "Y";
-
-	};
-
-	console.log("Thanks for playing! Bye!");
+	for (let i = 0; i < roundsToPlay; ++i) {
+		playerSelection = getPlayerSelection();
+		computerSelection = getComputerSelection();
+		result = playRound(playerSelection, computerSelection);
+		console.log(result);
+		if (result.includes("win")) { playerScore++; }
+		else if (result.includes("lose")) { computerScore++; }
+	}
+	//console.log(playerScore - computerScore);
+	if (playerScore - computerScore > 0)
+		console.log("Congratulations! You win the game!");
+	else if (playerScore - computerScore > 0)
+		console.log("Sorry. You lose the game!");
+	else
+		console.log("Scores drawn. No one wins.");
 }
+
+
 
 /************************************************/
 /* ---------- MAIN CODE STARTS HERE ----------- */
 /************************************************/
 
 const CHOICES = ["ROCK", "SCISSORS", "PAPER"];
-main();
+game();
